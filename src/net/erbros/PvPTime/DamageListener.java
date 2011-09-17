64627e83dc7a21f39c783891b65cf5db0e0d010a
Kent-Rune Henriksen <kentruneh@gmail.com>
2011-09-17 14:16:05 +0200

    Fixed the way we find out if it is pvp time or not.

--- src/net/erbros/PvPTime/DamageListener.java
+++ src/net/erbros/PvPTime/DamageListener.java
@@ -1,7 +1,6 @@
 package net.erbros.PvPTime;
 
 import org.bukkit.World;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -14,54 +13,49 @@ public class DamageListener extends EntityListener {
     	plugin = instance;
     }
     
-	public void onEntityDamage(EntityDamageEvent event) {
-		
-		if(event instanceof EntityDamageByEntityEvent) {
-			EntityDamageByEntityEvent entEvent = (EntityDamageByEntityEvent)event;
-			
-			// check if both are players. thanks LRFLEW :)
-			if(entEvent.getDamager() instanceof Player && entEvent.getEntity() instanceof Player) {
-				// Do we have a player with pvp override?
-				if(plugin.pvpOverrideEnabled == true) {
-					if(plugin.hasPermission((CommandSender) entEvent.getDamager(), "pvptime.override", true) == true) {
-						return;
-					}
-				}
-				Player attacker = (Player) entEvent.getDamager();
-				// What time is it? Thanks to sk89q :)
-				World world = attacker.getWorld();
-				long getTimeWorld = world.getTime();
-				//attacker.sendMessage(String.valueOf(getTimeWorld));
-				//two different solutions depending on which of the times are higher.
-				
-				if(getTimeWorld > plugin.pvpStartTime || getTimeWorld < plugin.pvpEndTime)
-				{
-					// What to do as pvp is clearly on.
-				}
-				else
-				{
-					// Give some info that pvp is off during day? Custom.
-					// Cancel the event.
-					event.setCancelled(true);
-				}
-			}
-			
-		}
-	}
-	
-	public boolean isItPvPTime(long time) {
-		if(plugin.pvpStartTime < plugin.pvpEndTime) {
-			if(time > plugin.pvpStartTime && time < plugin.pvpEndTime) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			if(time > plugin.pvpStartTime || time < plugin.pvpEndTime) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
+    @Override
+    public void onEntityDamage(EntityDamageEvent event) {
+
+            if(event instanceof EntityDamageByEntityEvent) {
+                EntityDamageByEntityEvent entEvent = (EntityDamageByEntityEvent)event;
+
+                // check if both are players. thanks LRFLEW :)
+                if(entEvent.getDamager() instanceof Player && entEvent.getEntity() instanceof Player) {
+                    // Do we have a player with pvp override?
+                Player attacker = (Player) entEvent.getDamager();
+                if(plugin.pvpOverrideEnabled == true) {
+                    if(attacker.hasPermission("pvptime.override")) {
+                            return;
+                    }
+                }
+
+                // What time is it? Thanks to sk89q :)
+                World world = attacker.getWorld();
+                long getTimeWorld = world.getTime();
+                //attacker.sendMessage(String.valueOf(getTimeWorld));
+                //two different solutions depending on which of the times are higher.
+
+                if(isItPvPTime(getTimeWorld) == false) {
+                    event.setCancelled(true);
+                }
+                return;
+            }
+        }
+    }
+
+    public boolean isItPvPTime(long time) {
+        if(plugin.pvpStartTime < plugin.pvpEndTime) {
+            if(time > plugin.pvpStartTime && time < plugin.pvpEndTime) {
+                    return true;
+            } else {
+                    return false;
+            }
+    } else {
+            if(time > plugin.pvpStartTime || time < plugin.pvpEndTime) {
+                    return true;
+            } else {
+                    return false;
+            }
+        }
+    }
 }
