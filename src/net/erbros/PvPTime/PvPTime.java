5b10b8ed9ecc916cd192d38aa74811c5e7708b2d
Kent-Rune Henriksen <kentruneh@gmail.com>
2012-07-10 22:37:50 +0200

    removed unused import

--- src/net/erbros/PvPTime/PvPTime.java
+++ src/net/erbros/PvPTime/PvPTime.java
@@ -9,7 +9,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.World;
 
 
